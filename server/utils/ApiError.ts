24262f4681864165a7cd9b02186ffcfc58f9543e
harshuh <harshh1124@gmail.com>
2026-07-06 18:48:28 +0530

    feat: implement ApiError class for structured error handling and add ApiResponse class for consistent API responses

--- server/utils/ApiError.ts
+++ server/utils/ApiError.ts
@@ -1,2 +1,19 @@
-// ApiError is used to extend errors  class to create in this file 
 
+export class ApiError extends Error {
+
+  public readonly statusCode: number;
+  public readonly isOperational: boolean;
+
+  constructor(
+    statusCode: number, 
+    message: string, 
+    isOperational: boolean = true) {
+        
+    super(message); 
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    Object.setPrototypeOf(this, ApiError.prototype); // instanceof ApiError check 
+    Error.captureStackTrace(this, this.constructor); // its used to clean Stack trace 
+  }
+}
